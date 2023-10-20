7268196042dccc2089ffcbbaeb567d327e34c7e4
Tom Lever <thomas.lever.business@gmail.com>
2023-10-20 13:58:10 -0400

    Modularized generate_feature_matrix_of_docking_scores_and_numbers_of_occurrences_of_substructures

--- Generate_Feature_Matrices.py
+++ Generate_Feature_Matrices.py
@@ -46,17 +46,23 @@ def calculate_array_of_values_of_descriptors(molecule):
 from rdkit import Chem
 import pandas as pd
 
-if __name__ == "__main__":
+def generate_feature_matrix_of_docking_scores_and_numbers_of_occurrences_of_substructures():
     list_of_columns = ['C' + str(i) for i in range(0, 1024)]
-    list_of_columns = ['Docking Score'] + list_of_columns
+    list_of_columns = ['Docking_Score'] + list_of_columns
     data_frame_of_docking_scores_and_SMILESs = pd.read_csv(filepath_or_buffer = 'Data_Frame_Of_Docking_Scores_And_SMILESs.csv')
     list_of_lists_of_docking_score_and_numbers_of_occurrences_of_substructures = []
     for i in range(0, 3):
         docking_score = data_frame_of_docking_scores_and_SMILESs.at[i, "docking score"]
         SMILES = data_frame_of_docking_scores_and_SMILESs.at[i, "SMILES"]
         molecule = Chem.MolFromSmiles(SMILES)
+        #molecule = Chem.MolFromSmiles('OC(=O)CN(CCN(CC(O)=O)CC(O)=O)CC(O)=O')
         list_of_numbers_of_occurrences_of_substructures = convert_to_list_of_numbers_of_occurrences_of_substructures(molecule)
         list_of_docking_score_and_numbers_of_occurrences_of_substructures = [docking_score] + list_of_numbers_of_occurrences_of_substructures
         list_of_lists_of_docking_score_and_numbers_of_occurrences_of_substructures.append(list_of_docking_score_and_numbers_of_occurrences_of_substructures)
     data_frame_of_docking_scores_and_numbers_of_occurrences_of_substructures = pd.DataFrame(list_of_lists_of_docking_score_and_numbers_of_occurrences_of_substructures, columns = list_of_columns)
-    print(data_frame_of_docking_scores_and_numbers_of_occurrences_of_substructures)
+    return data_frame_of_docking_scores_and_numbers_of_occurrences_of_substructures
+
+if __name__ == "__main__":
+        feature_matrix_of_docking_scores_and_numbers_of_occurrences_of_substructures = generate_feature_matrix_of_docking_scores_and_numbers_of_occurrences_of_substructures()
+        print(feature_matrix_of_docking_scores_and_numbers_of_occurrences_of_substructures)
+        feature_matrix_of_docking_scores_and_numbers_of_occurrences_of_substructures.to_csv('Feature_Matrix_Of_Docking_Scores_And_Number_Of_Occurrences_Of_Substructures.csv', index = False)
