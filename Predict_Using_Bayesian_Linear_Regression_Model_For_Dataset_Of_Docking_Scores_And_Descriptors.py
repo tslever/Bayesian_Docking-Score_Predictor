29132990d8b46ba1ecf69f537ed7832bba6720a8
Tom Lever <thomas.lever.business@gmail.com>
2023-10-26 00:38:34 -0400

    Create Predict_Using_Bayesian_Linear_Regression_Model_For_Dataset_Of_Docking_Scores_And_Descriptors.py

---
+++ Predict_Using_Bayesian_Linear_Regression_Model_For_Dataset_Of_Docking_Scores_And_Descriptors.py
@@ -0,0 +1,63 @@
+'''
+We use a PYMC model to predict docking scores based on test data not used for training, and compare averaged predicted docking scores and observed docking scores.
+
+Imports related to code before pymc_model:\n    pymc.set_data...
+'''
+import numpy as np
+import pandas as pd
+import pymc
+import pymc_bart
+
+# Imports related to code after pymc_model:\n    pymc.set_data...
+import matplotlib.pyplot as plt
+
+def main():
+
+    feature_matrix_of_docking_scores_and_values_of_descriptors = pd.read_csv(filepath_or_buffer = 'Feature_Matrix_Of_Docking_Scores_And_Values_Of_Descriptors.csv')
+    list_of_predictors = ['LabuteASA', 'MolLogP']
+    slice_of_feature_matrix_of_docking_scores_and_values_of_descriptors = feature_matrix_of_docking_scores_and_values_of_descriptors[list_of_predictors]
+    data_frame_of_values_of_predictors_for_training = slice_of_feature_matrix_of_docking_scores_and_values_of_descriptors.head(n = 100)
+    two_dimensional_array_of_values_of_predictors_for_training = data_frame_of_values_of_predictors_for_training.values
+    list_of_response = ['Docking_Score']
+    data_frame_of_values_of_response = feature_matrix_of_docking_scores_and_values_of_descriptors[list_of_response]
+    data_frame_of_values_of_response_for_training = data_frame_of_values_of_response.head(n = 100)
+    two_dimensional_array_of_values_of_response_for_training = data_frame_of_values_of_response_for_training.values
+    one_dimensional_array_of_values_of_response_for_training = two_dimensional_array_of_values_of_response_for_training.reshape(-1)
+
+    random_seed = 0
+    np.random.seed(random_seed)
+
+    with pymc.Model() as pymc_model:
+        MutableData_of_values_of_predictors = pymc.MutableData('MutableData_of_values_of_predictors', two_dimensional_array_of_values_of_predictors_for_training)
+        tensor_variable_representing_prior_probability_density_distribution_for_constant_term = pymc.Normal('constant_term', mu = 0, sigma = 10)
+        tensor_variable_representing_prior_probability_density_distribution_for_vector_of_coefficients = pymc.Normal('vector_of_coefficients', mu = 0, sigma = 10, shape = 2)
+        tensor_variable_representing_prior_probability_density_distribution_for_standard_deviation = pymc.HalfNormal('standard_deviation', sigma = 1)
+        tensor_variable_representing_expected_value_mu_of_outcomes = (
+            tensor_variable_representing_prior_probability_density_distribution_for_constant_term
+            + tensor_variable_representing_prior_probability_density_distribution_for_vector_of_coefficients[0] * MutableData_of_values_of_predictors[:, 0]
+            + tensor_variable_representing_prior_probability_density_distribution_for_vector_of_coefficients[1] * MutableData_of_values_of_predictors[:, 1]
+        )
+        tensor_variable_representing_likelihood_and_sampling_probability_density_distribution_of_outcomes = pymc.Normal('L(outcome | mu, sigma)', mu = tensor_variable_representing_expected_value_mu_of_outcomes, sigma = tensor_variable_representing_prior_probability_density_distribution_for_standard_deviation, observed = one_dimensional_array_of_values_of_response_for_training)
+        inference_data_with_samples_from_posterior_statistics_of_sampling_run_and_copy_of_observed_data = pymc.sample(random_seed = random_seed)
+
+    data_frame_of_values_of_predictors_for_testing = slice_of_feature_matrix_of_docking_scores_and_values_of_descriptors.tail(n = 100)
+    two_dimensional_array_of_values_of_predictors_for_testing = data_frame_of_values_of_predictors_for_testing.values
+    data_frame_of_values_of_response_for_testing = data_frame_of_values_of_response.tail(n = 100)
+    two_dimensional_array_of_values_of_response_for_testing = data_frame_of_values_of_response_for_testing.values
+    one_dimensional_array_of_values_of_response_for_testing = two_dimensional_array_of_values_of_response_for_testing.reshape(-1)
+
+    with pymc_model:
+        pymc.set_data({'MutableData_of_values_of_predictors': two_dimensional_array_of_values_of_predictors_for_testing})
+        array_of_predicted_docking_scores_4_chains_by_1000_samples_by_100_observations = pymc.sample_posterior_predictive(inference_data_with_samples_from_posterior_statistics_of_sampling_run_and_copy_of_observed_data)
+
+    array_of_averaged_predicted_docking_scores_100_observations_long = array_of_predicted_docking_scores_4_chains_by_1000_samples_by_100_observations.posterior_predictive['L(outcome | mu, sigma)'].mean(axis = (0, 1))
+    observed_docking_scores = one_dimensional_array_of_values_of_response_for_testing
+
+    fig = plt.figure(figsize = (12, 12))
+    ax = fig.add_subplot(projection = '3d')
+    ax.scatter(two_dimensional_array_of_values_of_predictors_for_training[:, 0], two_dimensional_array_of_values_of_predictors_for_training[:, 1], array_of_averaged_predicted_docking_scores_100_observations_long, color = 'blue')
+    ax.scatter(two_dimensional_array_of_values_of_predictors_for_testing[:, 0], two_dimensional_array_of_values_of_predictors_for_testing[:, 1], observed_docking_scores, color = 'red')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
