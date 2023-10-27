a6921222d1171d286a693acb6ec52c1aadbbb1cc
Tom Lever <thomas.lever.business@gmail.com>
2023-10-27 11:59:07 -0400

    Cleaned and modularized Plot_Baseline_And_Enrichment_Factor_Vs_Number_Of_Bins.py

--- Plot_Baseline_And_Enrichment_Factor_Vs_Number_Of_Bins.py
+++ Plot_Baseline_And_Enrichment_Factor_Vs_Number_Of_Bins.py
@@ -3,42 +3,36 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-data_frame = pd.read_csv('Data_Frame_Of_Observed_And_Averaged_Predicted_Docking_Scores_And_Indicators_That_Observation_Belongs_To_Lowest_10_Percent.csv')
-number_of_numbers_of_bike_rentals_in_lowest_10_percent = sum(data_frame['belongs_to_lowest_10_percent'])
-number_of_numbers_of_bike_rentals = data_frame.shape[0]
+data_frame_of_observed_and_averaged_predicted_docking_scores_and_indicators_that_observation_belongs_to_lowest_10_percent = pd.read_csv('Data_Frame_Of_Observed_And_Averaged_Predicted_Docking_Scores_And_Indicators_That_Observation_Belongs_To_Lowest_10_Percent.csv')
+column_belongs_to_lowest_10_percent = data_frame_of_observed_and_averaged_predicted_docking_scores_and_indicators_that_observation_belongs_to_lowest_10_percent['belongs_to_lowest_10_percent']
+number_of_docking_scores_in_lowest_10_percent = sum(column_belongs_to_lowest_10_percent)
+number_of_docking_scores = data_frame_of_observed_and_averaged_predicted_docking_scores_and_indicators_that_observation_belongs_to_lowest_10_percent.shape[0]
 number_of_bins = 50
-number_of_numbers_of_bike_rentals_per_bin = number_of_numbers_of_bike_rentals // number_of_bins
+number_of_docking_scores_per_bin = number_of_docking_scores // number_of_bins
 list_of_indices_of_bins = [i for i in range(0, number_of_bins)]
 list_of_baselines = []
-for i in list_of_indices_of_bins:
-    lower_index_of_bin = number_of_numbers_of_bike_rentals_per_bin * i
-    upper_index_of_bin = number_of_numbers_of_bike_rentals_per_bin * (i + 1) - 1
-    bin = data_frame[lower_index_of_bin : upper_index_of_bin]
-    number_of_numbers_of_bike_rentals_in_bin_in_lowest_10_percent = sum(bin['belongs_to_lowest_10_percent'])
-    number_of_numbers_of_bike_rentals_in_bin = bin.shape[0]
-    enrichment_factor = (number_of_numbers_of_bike_rentals_in_bin_in_lowest_10_percent / number_of_numbers_of_bike_rentals_in_lowest_10_percent) / (number_of_numbers_of_bike_rentals_in_bin / number_of_numbers_of_bike_rentals)
-    list_of_baselines.append(enrichment_factor)
 
-data_frame = data_frame.sort_values(by = 'averaged_predicted_docking_score', ascending = True)
-number_of_numbers_of_bike_rentals_in_lowest_10_percent = sum(data_frame['belongs_to_lowest_10_percent'])
-number_of_numbers_of_bike_rentals = data_frame.shape[0]
-number_of_numbers_of_bike_rentals_per_bin = number_of_numbers_of_bike_rentals // number_of_bins
+def append_baseline_or_enrichment_factor_to_list_of_baselines_or_enrichment_factors(data_frame, list_of_baselines_or_enrichment_factors):
+    for i in list_of_indices_of_bins:
+        lower_index_of_bin = number_of_docking_scores_per_bin * i
+        upper_index_of_bin = number_of_docking_scores_per_bin * (i + 1) - 1
+        bin = data_frame[lower_index_of_bin : upper_index_of_bin]
+        column_belongs_to_lowest_10_percent_in_bin = bin['belongs_to_lowest_10_percent']
+        number_of_docking_scores_in_bin_in_lowest_10_percent = sum(column_belongs_to_lowest_10_percent_in_bin)
+        number_of_docking_scores_in_bin = bin.shape[0]
+        baseline = (number_of_docking_scores_in_bin_in_lowest_10_percent / number_of_docking_scores_in_lowest_10_percent) / (number_of_docking_scores_in_bin / number_of_docking_scores)
+        list_of_baselines_or_enrichment_factors.append(baseline)
+
+append_baseline_or_enrichment_factor_to_list_of_baselines_or_enrichment_factors(data_frame_of_observed_and_averaged_predicted_docking_scores_and_indicators_that_observation_belongs_to_lowest_10_percent, list_of_baselines)
+data_frame_of_observed_and_averaged_predicted_docking_scores_and_indicators_that_observation_belongs_to_lowest_10_percent_ordered_by_averaged_predicted_docking_score = data_frame_of_observed_and_averaged_predicted_docking_scores_and_indicators_that_observation_belongs_to_lowest_10_percent.sort_values(by = 'averaged_predicted_docking_score', ascending = True)
 list_of_enrichment_factors = []
-for i in list_of_indices_of_bins:
-    lower_index_of_bin = number_of_numbers_of_bike_rentals_per_bin * i
-    upper_index_of_bin = number_of_numbers_of_bike_rentals_per_bin * (i + 1) - 1
-    bin = data_frame[lower_index_of_bin : upper_index_of_bin]
-    number_of_numbers_of_bike_rentals_in_bin_in_lowest_10_percent = sum(bin['belongs_to_lowest_10_percent'])
-    number_of_numbers_of_bike_rentals_in_bin = bin.shape[0]
-    enrichment_factor = (number_of_numbers_of_bike_rentals_in_bin_in_lowest_10_percent / number_of_numbers_of_bike_rentals_in_lowest_10_percent) / (number_of_numbers_of_bike_rentals_in_bin / number_of_numbers_of_bike_rentals)
-    list_of_enrichment_factors.append(enrichment_factor)
-
-data_frame = pd.DataFrame({
+append_baseline_or_enrichment_factor_to_list_of_baselines_or_enrichment_factors(data_frame_of_observed_and_averaged_predicted_docking_scores_and_indicators_that_observation_belongs_to_lowest_10_percent_ordered_by_averaged_predicted_docking_score, list_of_enrichment_factors)
+data_frame_of_indices_of_bins_values_of_baselines_or_enrichment_factors_and_specifications_of_baselines_or_enrichment_factos = pd.DataFrame({
     'index of bin': list_of_indices_of_bins + list_of_indices_of_bins,
     'value of baseline or enrichment factor': list_of_baselines + list_of_enrichment_factors,
-    'specification of baseline or enrichment factor': ['baseline' for i in range(0, number_of_bins)] + ['enrichment factor' for i in range(0, number_of_bins)]
+    'specification of baseline or enrichment factor': ['baseline' for _ in list_of_indices_of_bins] + ['enrichment factor' for _ in list_of_indices_of_bins]
 })
-sns.barplot(x = 'index of bin', y = 'value of baseline or enrichment factor', hue = 'specification of baseline or enrichment factor', data = data_frame)
+sns.barplot(x = 'index of bin', y = 'value of baseline or enrichment factor', hue = 'specification of baseline or enrichment factor', data = data_frame_of_indices_of_bins_values_of_baselines_or_enrichment_factors_and_specifications_of_baselines_or_enrichment_factos)
 plt.title('Value Of Baseline Or Enrichment Factor Vs. Index Of Bin')
 plt.xticks(rotation = 90)
 plt.show()
