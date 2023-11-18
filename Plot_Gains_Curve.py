260acd569810d02bb53650b38bb35f41d866047a
Tom Lever <thomas.lever.business@gmail.com>
2023-11-18 01:43:49 -0500

    Optimized Gains Curves

--- Plot_Gains_Curve.py
+++ Plot_Gains_Curve.py
@@ -9,29 +9,43 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
-
 def main(name_of_model, number_of_training_or_testing_observations, path_to_data_frame):
-    ax = None
-    for i in [2.5, 2.75, 3]:
-        df = pd.read_csv(path_to_data_frame)[['observed_response_value', 'average_of_predicted_response_values']]
-        data_frame_of_observed_response_values = df['observed_response_value']
-        the_mean = np.mean(data_frame_of_observed_response_values)
-        standard_deviation = np.std(data_frame_of_observed_response_values)
-        i_standard_deviations_below_mean = the_mean - i * standard_deviation
-        #ith_percentile_of_observed_response_values = np.percentile(data_frame_of_observed_response_values, i)
-        df['indicator_that_observed_response_value_is_in_lowest_i_percent'] = [1 if observed_response_value < i_standard_deviations_below_mean else 0 for observed_response_value in data_frame_of_observed_response_values]
+    dictionary_of_numbers_of_standard_deviations_below_mean_and_colors = {
+        -2.5: 'red',
+        -2.75: 'green',
+        -3: 'blue'
+    }
+    data_frame_of_observed_response_values_and_average_of_predicted_response_values = pd.read_csv(path_to_data_frame)[['observed_response_value', 'average_of_predicted_response_values']]
+    series_of_observed_response_values = data_frame_of_observed_response_values_and_average_of_predicted_response_values['observed_response_value']
+    for pair in dictionary_of_numbers_of_standard_deviations_below_mean_and_colors.items():
+        df = data_frame_of_observed_response_values_and_average_of_predicted_response_values[['observed_response_value']].copy()
+        z_score = pair[0]
+        threshold = np.mean(series_of_observed_response_values) + z_score * np.std(series_of_observed_response_values)
+        df['indicator_that_observed_response_value_is_below_threshold'] = [1 if observed_response_value < threshold else 0 for observed_response_value in series_of_observed_response_values]
         df = df.sort_values(by = 'observed_response_value')
-        df['index'] = np.arange(0, len(df.index))
-        df['ideal_cumulative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'] = np.cumsum(df['indicator_that_observed_response_value_is_in_lowest_i_percent'])
-        df['ideal_cumulative_relative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'] = df['ideal_cumulative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'] / max(df['ideal_cumulative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'])
-        ax = df.plot(ax = ax, x = 'index', y = 'ideal_cumulative_relative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent', label = f'ideal; i = {i}')
+        df.reset_index(drop = True, inplace = True)
+        df.drop('observed_response_value', axis = 1, inplace = True)
+        df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'] = np.cumsum(df['indicator_that_observed_response_value_is_below_threshold'])
+        df.drop('indicator_that_observed_response_value_is_below_threshold', axis = 1, inplace = True)
+        df['cumulative_relative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'] = df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'] / max(df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'])
+        df.drop('cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model', axis = 1, inplace = True)
+        print(df.head(n = 3))
+        plt.plot(df.index, df['cumulative_relative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'], label = f'perfect model; z score = {z_score}', color = pair[1])
+        df = data_frame_of_observed_response_values_and_average_of_predicted_response_values.copy()
+        df['indicator_that_observed_response_value_is_below_threshold'] = [1 if observed_response_value < threshold else 0 for observed_response_value in series_of_observed_response_values]
+        df.drop('observed_response_value', axis = 1, inplace = True)
         df.sort_values(by = 'average_of_predicted_response_values', inplace = True)
-        df['index'] = np.arange(0, len(df.index))
-        df['actual_cumulative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'] = np.cumsum(df['indicator_that_observed_response_value_is_in_lowest_i_percent'])
-        df['actual_cumulative_relative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'] = df['actual_cumulative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'] / max(df['actual_cumulative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent'])
-        ax = df.plot(ax = ax, x = 'index', y = 'actual_cumulative_relative_frequency_of_indicators_that_observed_response_value_is_in_lowest_i_percent', label = f'actual; i = {i}')
+        df.reset_index(drop = True, inplace = True)
+        df.drop('average_of_predicted_response_values', axis = 1, inplace = True)
+        df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_trained_model'] = np.cumsum(df['indicator_that_observed_response_value_is_below_threshold'])
+        df.drop('indicator_that_observed_response_value_is_below_threshold', axis = 1, inplace = True)
+        df['cumulative_relative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_trained_model'] = df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_trained_model'] / max(df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_trained_model'])
+        df.drop('cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_trained_model', axis = 1, inplace = True)
+        print(df.head(n = 3))
+        plt.plot(df.index, df['cumulative_relative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_trained_model'], label = f'trained model; z score = {z_score}', color = pair[1])
     plt.grid()
-    plt.title(f'Cumulative Relative Frequency\nOf Indicators That Observed Response Value Is In Lowest i Percent\nVs. Index\nIn Data Frame Of {number_of_training_or_testing_observations} Indicators Sorted By Average Of Predicted Response Values\nFor {name_of_model}', fontsize = 8)
+    plt.legend()
+    plt.title(f'Cumulative Relative Frequency\nOf Indicators That Observed Response Value Is Below Threshold Determined By z Score\nVs. Index\nIn Data Frame Of {number_of_training_or_testing_observations} Indicators Sorted By Average Of Predicted Response Values\nFor {name_of_model}', fontsize = 10)
     plt.xlabel('Index')
     plt.ylabel('Cumulative Relative Frequency')
     plt.show()
